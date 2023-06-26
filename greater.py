73050e6240393c9209f641ff974acfb5a65df8c6
Raichz <78966888+raichalot7@users.noreply.github.com>
2023-06-26 10:46:11 +0530

    Add files via upload

---
+++ greater.py
@@ -0,0 +1,11 @@
+#To find the largest among 3 numbers
+
+a=int(input("Enter the First value :"))
+b=int(input("Enter the Second value :"))
+c=int(input("Enter the Third value :"))
+if a>b and a>c:
+    print("a value is greater ")
+if b>a and b>c:
+    print("b value is greater ")
+if c>a and c>b:
+    print("c value is greater ")
